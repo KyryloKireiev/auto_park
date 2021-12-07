f8ac0f5eb4a5b1ecbb4a86ba06f02f4a21e853cc
KyryloKireiev <90455454+KyryloKireiev@users.noreply.github.com>
2021-12-07 21:35:38 +0200

    with driver filter works

--- auto_park/park/urls.py
+++ auto_park/park/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import DriverListAPIView, DriverDetailCRUDAPIView, VehicleListAPIView, VehicleDetailCRUDAPIView
+from .views import DriverListAPIView, \
+    DriverDetailCRUDAPIView, VehicleListAPIView, \
+    VehicleDetailCRUDAPIView, VehicleCreateDestroyDriverAPIView
 
 app_name = "park"
 # app_name will help us do a reverse look-up latter.
@@ -7,5 +9,6 @@ urlpatterns = [
     path('driver/', DriverListAPIView.as_view(), name='driver_list'),
     path('driver/<str:id>/', DriverDetailCRUDAPIView.as_view(), name='driver_detail'),
     path('vehicle/', VehicleListAPIView.as_view(), name='vehicle_list'),
-    path('vehicle/<str:id>', VehicleDetailCRUDAPIView.as_view(), name='vehicle_detail'),
+    path('vehicle/<str:id>/', VehicleDetailCRUDAPIView.as_view(), name='vehicle_detail'),
+    path('set_driver/<str:id>/', VehicleCreateDestroyDriverAPIView.as_view(), name='create_destroy_driver'),
 ]
